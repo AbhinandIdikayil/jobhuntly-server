93a787691238e52a68f4066f20b5018ae406c353
Abhinand <abhin4nd6@gmail.com>
2024-07-09 18:16:41 +0530

    Remove build directory from Git tracking

--- services/user/build/infratructure/rabbitmq/instance.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RABBIT_MQ = void 0;
-const _1 = require(".");
-exports.RABBIT_MQ = _1.RabbitMQClient.getInstance();
