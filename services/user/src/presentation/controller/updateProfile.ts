90744e6d028e4c53dddad5dcd1530bc2df4b9fab
Abhinand <abhin4nd6@gmail.com>
2024-07-30 13:17:56 +0530

    user updating social link done

--- services/user/src/presentation/controller/updateProfile.ts
+++ services/user/src/presentation/controller/updateProfile.ts
@@ -7,22 +7,38 @@ export const updateProfileController = (dependencies: IDependencies) => {
     const { usecases: { updateProfileUsecase } } = dependencies
     return async (req: ModifiedRequest, res: Response, next: NextFunction) => {
         try {
-            const {email} = req.user || {}
-            if (req.body) {
-                let data = {
+            const { email } = req.user || {}
+            if (!req.body) {
+                return res.status(400).json('Request body is empty');
+            }
+
+            let data
+            let result
+            if (req.body.twitter || req.body.instagram || req.body.linkedin || req.body.personalwebsite) {
+                data = {
+                    socialLink: [
+                        req.body?.twitter || '',
+                        req.body?.linkedin || '',
+                        req.body?.instagram || '',
+                    ],
+                    email,
+                    personalsite:req?.body?.personalsite || '',
+                }
+                result = await updateProfileUsecase(dependencies).execute(data);
+            } else {
+                data = {
                     ...req.body,
                     email
                 }
-                let result = await updateProfileUsecase(dependencies).execute(data);
-                if (result) {
-                    axios.post(`${process.env.JOB_SERVICE_URL}/add-user`,result)
-                    return res.status(200).json(result);
-                } else {
-                    return res.status(404).json('Error while updating')
-                }
-            } else {
-                return res.status(404).json('req body is empty')
+                result = await updateProfileUsecase(dependencies).execute(data);
             }
+            if (result) {
+                // axios.post(`${process.env.JOB_SERVICE_URL}/add-user`,result)
+                return res.status(200).json(result);
+            } else {
+                return res.status(404).json('Error while updating')
+            }
+
         } catch (error) {
             next(error)
         }
