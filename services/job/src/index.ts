5a4a92a80a236204cb44ad7035bdf45a7207fde6
Abhinand <abhin4nd6@gmail.com>
2024-09-19 20:43:48 +0530

    fix: fix in job srv

--- services/job/src/index.ts
+++ services/job/src/index.ts
@@ -1,8 +1,8 @@
 import { connectDB } from "./config/config"
 import { startProducer } from "./config/rabbitmq"
 import app from "./presentation/server"
-import { resumeAnalyzer } from "./utils/resumeAnalyzer";
-import { analyzeResume } from "./utils/resumeAnalyzer2";
+// import { resumeAnalyzer } from "./utils/resumeAnalyzer";
+// import { analyzeResume } from "./utils/resumeAnalyzer2";
 import { main } from "./utils/tf.train";
 
 const cloudinaryUrl = 'https://res.cloudinary.com/dghv07eag/image/upload/v1722945870/jobhunty/fazal_mpuocj.pdf';
