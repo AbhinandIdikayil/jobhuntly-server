c2d8779e984c83a42afef9a4d9a4aac94f0ba0f2
Abhinand <abhin4nd6@gmail.com>
2024-09-21 14:30:25 +0530

    fix: fix in refresh token

--- services/auth/src/presentation/controller/login.ts
+++ services/auth/src/presentation/controller/login.ts
@@ -21,7 +21,7 @@ export const loginContoller = (dependencies: IDependencies) => {
                 const token = generateToken({ _id: String(result?._id), email: result?.email, role: result?.role ?? '' })
                 const refreshToken = generateRefreshToken({ _id: String(result?._id), email: result?.email, role: result?.role ?? '' })
                 return res.status(200)
-                    .cookie('access_token', token, { maxAge: maxAge?.maxAge})
+                    .cookie('access_token', token, {httpOnly: true, sameSite:'none', secure:true , maxAge: maxAge?.maxAge})
                     .cookie('refresh_token', refreshToken, { httpOnly: true, sameSite:'none', secure:true , maxAge: refreshTokenMaxage?.maxAge })
                     .json(result)
 
