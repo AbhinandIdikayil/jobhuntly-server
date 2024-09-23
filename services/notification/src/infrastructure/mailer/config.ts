11154c8434e204a044ae3b711275d5b14ac0dbb1
Abhinand <abhin4nd6@gmail.com>
2024-09-23 12:09:12 +0530

    fix: fix in notification

--- services/notification/src/infrastructure/mailer/config.ts
+++ services/notification/src/infrastructure/mailer/config.ts
@@ -4,9 +4,9 @@ config()
 
 export const transporter = nodemailer.createTransport({
     service: "gmail",
-    // host:'smtp.gmail.com',
-    // port: 465,
-    // secure: true,
+    host:'smtp.gmail.com',
+    port: 465,
+    secure: true,
     // logger: true,
     // debug: true,
     // secureConnection: false,
